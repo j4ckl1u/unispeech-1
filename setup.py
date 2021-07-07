7087ed99c27681e761a66da68dc53d883688c05d
cywang <cywang@mail.nankai.edu.cn>
2021-07-07 09:07:48 +0000

    small update

--- setup.py
+++ setup.py
@@ -224,8 +224,8 @@ def get_files(path, relative_to="fairseq"):
 try:
     # symlink config and examples into fairseq package so package_data accepts them
     if "build_ext" not in sys.argv[1:]:
-        os.symlink(os.path.join("..", "config"), "unispeech/config")
-        os.symlink(os.path.join("..", "examples"), "unispeech/examples")
+        os.symlink(os.path.join("..", "config"), "fairseq/config")
+        os.symlink(os.path.join("..", "examples"), "fairseq/examples")
     package_data = {
         "fairseq": get_files("fairseq/config") + get_files("fairseq/examples"),
     }
